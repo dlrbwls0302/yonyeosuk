c629d7b5d402d1f2fa71ff827dab17811c43579c
Kyu Jin Lee <dcohornt@gmail.com>
2021-03-17 15:46:10 +0900

    create Models and Migrations/completed/테이블에 들어갈 필드와 FK 지정완료

---
+++ migrations/20210317061249-create-board.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('board', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      users_id: {
+        type: Sequelize.INTEGER
+      },
+      like: {
+        type: Sequelize.INTEGER
+      },
+      dislike: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('board');
+  }
+};
