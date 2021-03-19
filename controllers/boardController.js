c2089156b046d1ebe40faca5632ea7d17b427402
juyeop <shininggiver@gmail.com>
2021-03-19 15:42:23 +0900

    feature/writepost/working/writepost api 수정

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -15,38 +15,36 @@ module.exports = {
                 title: title,
                 description: description,
                 users_id: req.params.id
-            })
-            res.send('testing')
-            console.log(usersPostId)
-
+            }).dataValues.id
+            // images가 있을때와 undefined일 때 분기를 해야함!
             const images = req.files;
-            const path = images.map(image => {
-                return {
-                    image: image.path,
-                    board_id: usersPostId
-                }
-            });
-            //[{}, {}, {}]
-            // path들을 데이터베이스 image 의 path필드에 넣자
-            if (path.length !== 0) {
-                image.bulkCreate(path)
-                .then(res => {
-                    res.status(201).json({
-                        message: 'Successfully created!'
-                    })
-                })
-                .catch(err => {
-                    if (err) {
-                        res.status(500).json({
-                            message: 'Server error has occurred!'
-                        })
+            if (images) {
+                const path = images.map(image => {
+                    return {
+                        image: image.path,
+                        board_id: usersPostId
                     }
-                })
+                });
+                if (path.length !== 0) {
+                    image.bulkCreate(path)
+                    .then(res => {
+                        res.status(201).json({
+                            message: 'Successfully created!'
+                        })
+                    })
+                    .catch(err => {
+                        if (err) {
+                            res.status(500).json({
+                                message: 'Server error has occurred!'
+                            })
+                        }
+                    })
+                } 
             } else {
                 res.status(201).json({
                     message: 'Successfully created but images are none'
                 })
-            } 
+            }
         } else {
             res.status(400).json({
                 message: "제목과 본문은 필수 항목입니다!"
