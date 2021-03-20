d6057434a1014fa7f1eeaf1b679b133d060cce80
Ubuntu <ubuntu@ip-172-31-41-203.ap-northeast-2.compute.internal>
2021-03-20 09:27:58 +0000

    fixed/schema/working/ec2 schema test ok

--- migrations/20210320082051-fix_schema.js
+++ migrations/20210320082051-fix_schema.js
@@ -28,5 +28,27 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
+    await queryInterface.addConstraint('users_items', {
+      fields: ['users_id'],
+      type: 'foreign key',
+      name: 'custom_fkey_constraint_users_id',
+      references: {
+        table: 'users',
+        field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    });
+    await queryInterface.addConstraint('users_items', {
+      fields: ['item_id'],
+      type: 'foreign key',
+      name: 'custom_fkey_constraint_item_id',
+      references: {
+        table: 'items',
+        field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    });
   }
 };
